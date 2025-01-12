1c84abdf5e092e8c2c37d59e3e267950e7b0a232
Dan Velton <48307985+dvelton@users.noreply.github.com>
2025-01-12 14:00:58 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-function App() {
-  return <h1>Hello, React!</h1>;
-}
+import App from './script';
 
 ReactDOM.render(<App />, document.getElementById('root'));
